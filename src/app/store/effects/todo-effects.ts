d0a211d4e655f1aa063f69bcf4616368f954223d
Curtis Schlak <curtis.schlak@hackreactor.com>
2018-04-18 16:15:28 -0400

    Add async listener that generates actions.

--- src/app/store/effects/todo-effects.ts
+++ src/app/store/effects/todo-effects.ts
@@ -3,12 +3,21 @@ import { State } from '../state';
 import { ADD_TODO, AddTodo, AddTodoCompleted, LOAD_TODOS, LoadTodos, LoadTodosCompleted } from '../actions/todo-actions';
 import { ToDoItem } from '../../models/todo';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class ToDoEffects {
   constructor(
-    private _actions: Actions
-  ) {}
+    private _actions: Actions,
+    store: Store<State>
+  ) {
+    window.addEventListener('storage', event => {
+      if (event.key === 'items') {
+        const items = JSON.parse(event.newValue);
+        store.dispatch(new LoadTodosCompleted(items));
+      }
+    });
+  }
 
   @Effect() addTodo$ = this._actions
     .ofType(ADD_TODO)
